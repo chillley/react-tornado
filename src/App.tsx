a7e345829fba29f9442ca0c768e3dcd0fc719595
chillley <chillley@163.com>
2022-01-17 16:47:53 +0800

    add craco.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'antd';
+
 
 function App() {
     return (
@@ -18,6 +20,7 @@ function App() {
                 >
                     Learn React
                 </a>
+                <Button>1111111111</Button>
             </header>
         </div>
     );
