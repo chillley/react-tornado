b06e808426b11e9ab58df79c32964fc210d0c141
chillley <chillley@163.com>
2022-01-18 10:29:54 +0800

    update craco.config.ts

--- craco.config.ts
+++ craco.config.ts
@@ -1,7 +1,24 @@
+import WebpackBar from "webpackbar"
+// const {whenDev} = require('@craco/craco')
+
+
 // @ts-ignore
 module.exports = {
-    
-
-
-
+    babel: {
+        presets: [[
+            '@babel/preset-env'
+        ]],
+        plugins: []
+    },
+    webpack: {
+        plugins: [
+            /** 不会吧,不会还有人不喜欢粉红色? */
+            new WebpackBar({color: "pink"})
+        ]
+    },
+    devServer: {
+        open: false,
+        port: 8082,
+        proxy: {}
+    }
 }
