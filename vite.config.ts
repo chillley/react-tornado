afed42d681112a4fe6c13e54f83d1774eef82e55
chillley <chillley@163.com>
2022-01-24 17:58:22 +0800

    add arco-design

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginForArco from '@arco-plugins/vite-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    resolve: {
+        alias: [
+            {find: "@", replacement: "/src"},
+        ],
+    },
+    plugins: [react(),vitePluginForArco()],
+    server: {
+        host: true,
+        port: 8082,
+        proxy: {}
+    }
 })
